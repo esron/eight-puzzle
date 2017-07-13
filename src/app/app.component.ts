13a96a125d0e47456f873b8c6900b668dc0cee01
Esron Silva <esron.dtamar@gmail.com>
2017-07-12 22:13:15 -0300

    Classe State, animação do embaralhamento, esqueleto da expansão da borda.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { element } from 'protractor';
 import { Component } from '@angular/core';
+
+import { State } from './state';
 
 @Component({
   selector: 'app-root',
@@ -7,99 +10,80 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-
-  // Title
+  puzzle: State;
   title = "Eight Puzzle";
-
-  // Estado alvo
-  tagetState: number[][] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
-
-  // Inicializa o puzzle com o estado alvo
-  puzzle: number[][] = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 
   // Variaveis auxiliares
   emptyCell: number = 9;
 
+  constructor() {
+    // Inicializa o puzzle com o estado alvo
+    this.puzzle = new State([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+    // this.shuflle(50);
+  }
+
 
   // Executa um movimento
   movement(cell: number, i: number, j: number) {
+    console.log("Cliccou ne célula " + cell);
     switch (cell) {
       case 1: {
-        this.swipe(0, 0, 0, 1);
-        this.swipe(0, 0, 1, 0);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(0, 0, 0, 1);
+        this.puzzle.swipe(0, 0, 1, 0);
         break;
       }
       case 2: {
-        this.swipe(0, 1, 0, 0);
-        this.swipe(0, 1, 1, 1);
-        this.swipe(0, 1, 0, 2);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(0, 1, 0, 0);
+        this.puzzle.swipe(0, 1, 1, 1);
+        this.puzzle.swipe(0, 1, 0, 2);
         break;
       }
       case 3: {
-        this.swipe(0, 2, 0, 1);
-        this.swipe(0, 2, 1, 2);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(0, 2, 0, 1);
+        this.puzzle.swipe(0, 2, 1, 2);
         break;
       }
       case 4: {
-        this.swipe(1, 0, 0, 0);
-        this.swipe(1, 0, 1, 1);
-        this.swipe(1, 0, 2, 0);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(1, 0, 0, 0);
+        this.puzzle.swipe(1, 0, 1, 1);
+        this.puzzle.swipe(1, 0, 2, 0);
         break;
       }
       case 5: {
-        this.swipe(1, 1, 0, 1);
-        this.swipe(1, 1, 1, 0);
-        this.swipe(1, 1, 1, 2);
-        this.swipe(1, 1, 2, 1);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(1, 1, 0, 1);
+        this.puzzle.swipe(1, 1, 1, 0);
+        this.puzzle.swipe(1, 1, 1, 2);
+        this.puzzle.swipe(1, 1, 2, 1);
         break;
       } 
       case 6: {
-        this.swipe(1, 2, 0, 2);
-        this.swipe(1, 2, 1, 1);
-        this.swipe(1, 2, 2, 2);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(1, 2, 0, 2);
+        this.puzzle.swipe(1, 2, 1, 1);
+        this.puzzle.swipe(1, 2, 2, 2);
         break;
       }
       case 7: {
-        this.swipe(2, 0, 1, 0);
-        this.swipe(2, 0, 2, 1);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(2, 0, 1, 0);
+        this.puzzle.swipe(2, 0, 2, 1);
         break;
       }
       case 8: {
-        this.swipe(2, 1, 2, 0);
-        this.swipe(2, 1, 1, 1);
-        this.swipe(2, 1, 2, 2);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(2, 1, 2, 0);
+        this.puzzle.swipe(2, 1, 1, 1);
+        this.puzzle.swipe(2, 1, 2, 2);
         break;
       }
       case 9: {
-        this.swipe(2, 2, 1, 2);
-        this.swipe(2, 2, 2, 1);
-        console.log("Cliccou em " + cell + " puzzle["+i+"]["+j+"]: " + this.puzzle[i][j]);
+        this.puzzle.swipe(2, 2, 1, 2);
+        this.puzzle.swipe(2, 2, 2, 1);
         break;
       }
-    }
-  }
-  
-  // Tenta trocar o ponto (x1, y1) pelo ponto (x2, y2)
-  swipe(x1: number, y1: number, x2: number, y2: number) {
-    if (this.puzzle[x2][y2] == 9) {
-      console.log("Trocou: "+ this.puzzle[x1][y1]+", por "+ this.puzzle[x2][y2]);
-      this.puzzle[x2][y2] = this.puzzle[x1][y1];
-      this.puzzle[x1][y1] = 9;
-      this.emptyCell = x1 * 3 + y1 + 1;
     }
   }
 
   // Testa se uma celula pode ser visivel
   isVisible(i: number, j: number) {
-    return this.puzzle[i][j] != 9;
+    return this.puzzle.board[i][j] != 9;
   }
   
   randomNumber(n) {
@@ -107,100 +91,162 @@ export class AppComponent {
   }
 
   randomMove() {
-    switch(this.emptyCell) {
+    switch(this.puzzle.emptyCell) {
       case 1: {
         let move: number = this.randomNumber(2);
         if(move == 0)       
-          this.swipe(0, 1, 0, 0);
+          this.puzzle.swipe(0, 1, 0, 0);
         else
-          this.swipe(1, 0, 0, 0);
+          this.puzzle.swipe(1, 0, 0, 0);
         break;
       }
       case 2: {
         let move: number = this.randomNumber(3);
         if(move == 0)
-          this.swipe(0, 0, 0, 1);
+          this.puzzle.swipe(0, 0, 0, 1);
         else if(move ==1)
-          this.swipe(1, 1, 0, 1);
+          this.puzzle.swipe(1, 1, 0, 1);
         else
-          this.swipe(0, 2, 0, 1);
+          this.puzzle.swipe(0, 2, 0, 1);
         break;
       }
       case 3: {
         let move: number = this.randomNumber(2);
         if(move == 0)
-          this.swipe(0, 1, 0, 2);
+          this.puzzle.swipe(0, 1, 0, 2);
         else 
-          this.swipe(1, 2, 0, 2);
+          this.puzzle.swipe(1, 2, 0, 2);
         break;
       }
       case 4: {
         let move: number = this.randomNumber(3);
         if(move == 0)
-          this.swipe(0, 0, 1, 0);
+          this.puzzle.swipe(0, 0, 1, 0);
         else if(move == 1)
-          this.swipe(1, 1, 1, 0);
+          this.puzzle.swipe(1, 1, 1, 0);
         else
-          this.swipe(2, 0, 1, 0);
+          this.puzzle.swipe(2, 0, 1, 0);
         break;
       }
       case 5: {
         let move: number = this.randomNumber(4);
         if(move == 0)
-          this.swipe(0, 1, 1, 1);
+          this.puzzle.swipe(0, 1, 1, 1);
         else if(move == 1)
-          this.swipe(1, 0, 1, 1);
+          this.puzzle.swipe(1, 0, 1, 1);
         else if(move == 2)
-          this.swipe(1, 2, 1, 1);
+          this.puzzle.swipe(1, 2, 1, 1);
         else 
-          this.swipe(2, 1, 1, 1);
+          this.puzzle.swipe(2, 1, 1, 1);
         break;
       } 
       case 6: {
         let move: number = this.randomNumber(3);
         if(move == 0)
-          this.swipe(0, 2, 1, 2);
+          this.puzzle.swipe(0, 2, 1, 2);
         else if(move == 1)
-          this.swipe(1, 1, 1, 2);
+          this.puzzle.swipe(1, 1, 1, 2);
         else
-          this.swipe(2, 2, 1, 2);
+          this.puzzle.swipe(2, 2, 1, 2);
         break;
       }
       case 7: {
         let move: number = this.randomNumber(2);
         if(move == 0)
-          this.swipe(1, 0, 2, 0);
+          this.puzzle.swipe(1, 0, 2, 0);
         else
-          this.swipe(2, 1, 2, 0);
+          this.puzzle.swipe(2, 1, 2, 0);
         break;
       }
       case 8: {
         let move: number = this.randomNumber(3);
         if(move == 0)
-          this.swipe(2, 0, 2, 1);
+          this.puzzle.swipe(2, 0, 2, 1);
         else if(move == 1)
-          this.swipe(1, 1, 2, 1);
+          this.puzzle.swipe(1, 1, 2, 1);
         else
-          this.swipe(2, 2, 2, 1);
+          this.puzzle.swipe(2, 2, 2, 1);
         break;
       }
       case 9: {
         let move: number = this.randomNumber(2);
-        
         if(move == 0)
-          this.swipe(1, 2, 2, 2)
+          this.puzzle.swipe(1, 2, 2, 2)
         else 
-          this.swipe(2, 1, 2, 2);
+          this.puzzle.swipe(2, 1, 2, 2);
         break;
       }
     }
   }
 
   shuflle(n: number) {
-    console.log("Embaralhando");
-    for(let i = 0; i < n; i++) {
-      console.log("Embaralhando "+ i +", celula vazia: " + this.emptyCell);
-      this.randomMove();
+    if(n != 0){
+        console.log("Embaralhando "+ (50 - n) +", celula vazia: " + this.puzzle.emptyCell);
+        this.randomMove();
+        window.setTimeout(() => this.shuflle(n - 1), 100);
     }
+  }
+
+  compareHeuristc(a: State, b: State) {
+    return a.heuristic - b.heuristic;
+  }
+
+  // Gera os estados a partir de um estado e adiciona à borda
+  expandBorder(state: State) {
+    // Lista auxiliar
+    let lst: State[] = [];
+
+    // Variável auxíliar
+    let newState: State;
+
+    switch(state.emptyCell) {
+      case 1: {
+        newState = new State(state.board);
+        lst.push(newState.swipe(0, 1, 0, 0))
+        newState = new State(state.board);
+        lst.push(newState.swipe(1, 0, 0, 0))
+        return lst;
+      }
+      case 2: {
+        return lst;
+      }
+      case 3: {
+        return lst;
+      }
+      case 4: {
+        return lst;
+      }
+      case 5: {
+        return lst;
+      }
+      case 6: {
+        return lst;
+      }
+      case 7: {
+        return lst;
+      }
+      case 8: {
+        return lst;
+      }
+      case 9: {
+        return lst;
+      }
+    }
+  }
+
+  solvePuzzle() {
+    console.log("Resolvendo");
+
+    // Estado corrente
+    let currentState: State = new State(this.puzzle.board);
+
+    // Borda
+    let border: State[] = [];
+
+    this.expandBorder(currentState).forEach(element => { border.push(element) });
+
+    border.sort(this.compareHeuristc);
+
+    border.forEach(element => { element.logBoard() });
   }
 }
---
+++ src/app/state.ts
@@ -0,0 +1,83 @@
+export class State {
+    board: number[][] = [];
+    heuristic: number = 0;
+    emptyCell: number;
+
+    constructor(board: number[][]) {
+        for (let i = 0; i < 3; i++) {
+            this.board[i] = [];
+            for (let j = 0; j < 3; j++) {
+                this.board[i][j] = board[i][j];
+            }
+        }
+
+        this.hn();
+    }
+
+    private manhathanDistance(A: number[], B: number[]) {
+        return Math.abs(B[0] - A[0]) + Math.abs(B[1] - A[1]);
+    }
+
+    private hn() {
+        let newHeuristic: number = 0;
+        for (let i = 0; i < 3; i++)
+            for (let j = 0; j < 3; j++) {
+                switch(this.board[i][j]) {
+                    case 1: {
+                        newHeuristic += this.manhathanDistance([i, j], [0, 0]);
+                        break;
+                    }
+                    case 2: {
+                        newHeuristic += this.manhathanDistance([i, j], [0, 1]);
+                        break;
+                    }
+                    case 3: {
+                        newHeuristic += this.manhathanDistance([i, j], [0, 2]);
+                        break;
+                    }
+                    case 4: {
+                        newHeuristic += this.manhathanDistance([i, j], [1, 0]);
+                        break;
+                    }
+                    case 5: {
+                        newHeuristic += this.manhathanDistance([i, j], [1, 1]);
+                        break;
+                    }
+                    case 6: {
+                        newHeuristic += this.manhathanDistance([i, j], [1, 2]);
+                        break;
+                    }
+                    case 7: {
+                        newHeuristic += this.manhathanDistance([i, j], [2, 0]);
+                        break;
+                    }
+                    case 8: {
+                        newHeuristic += this.manhathanDistance([i, j], [2, 1]);
+                        break;
+                    }
+                    case 9: {
+                        this.emptyCell = 3 * i + j + 1;
+                    }
+                }
+            }
+        this.heuristic = newHeuristic;
+    }
+
+    // Tenta trocar o ponto (x1, y1) pelo ponto (x2, y2)
+    swipe(x1: number, y1: number, x2: number, y2: number) {
+        if (this.board[x2][y2] == 9) {
+            console.log("Trocou: "+ this.board[x1][y1]+", por "+ this.board[x2][y2]);
+            this.board[x2][y2] = this.board[x1][y1];
+            this.board[x1][y1] = 9;
+            this.emptyCell = x1 * 3 + y1 + 1;
+        }
+        this.hn();
+        return this;
+    }
+
+    logBoard() {
+       for (let i = 0; i < 3; i++)
+        console.log(this.board[i][0] + " " + this.board[i][1] + " " + this.board[i][2]);
+       console.log("h(n) =  "+ this.heuristic +"\n");
+    }
+}
